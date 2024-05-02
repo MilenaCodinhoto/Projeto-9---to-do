5e30e531750d66c37187cd43e65c755f782b05d0
Milena <milena.cod@gmail.com>
2024-05-02 11:21:16 -0300

    criando a função remover

--- Desktop/projeto9/src/js/script.js
+++ Desktop/projeto9/src/js/script.js
@@ -1,30 +1,49 @@
-//declarando a variavel
+//Declarando as variaveis
+let tarefas=[];
 
-let tarefa = [];
+//Função que valida o preenchimento do campo caso esteja vazio
 
-//função que valida o preenchimento do campo caso esteja vazio
-
-function validaCampo(){
-    let valida = false;
-    if(document.getElementById("task").value =="");
-    valida = true;
+function validaCampo()
+{
+    let valida =false;
+    if(document.getElementById("task").value =="") 
+    valida =true;
     return valida;
 }
-
-//função que adiciona a tarefa
+//Função que adiciona a tarefa
 
 function adicionarTarefa(){
-    //criando a variavel q vai receber as tarefas
-    let linhas = document.getElementById("task");
+        //criando a variavel que vai receber as tarefas
+    let linhas =document.getElementById("task");
+
     if(validaCampo()){
-        alert("Preencha o campo")
+        alert("Preencha o campo com alguma Tarefa")
     }else{
-        // add tarefas
-        tarefa.push(linhas.value);
-        //passando para a proxima
+        //adicionando as tarefas
+        tarefas.push(linhas.value);
+        //passando para a proxima linha vazia
         linhas.value="";
-        //chamando a função q vai mostrar todas as tarefas
+        //chamando a função que vai mostrar todas as tarefas
         listarTarefas();
     }
+    //volta para o input da tarefa
+    document.getElementById("task").focus();
+}
 
+//Função Listar tarefas
+
+function listarTarefas(){
+    let valor="";
+    for(let i=0; i<tarefas.length;i++){
+        valor += tarefas[i] +"<br>";
+    }
+    document.getElementById("lista").innerHTML =valor;
+}
+
+//função remover tarefa
+
+function removerTarefa(){
+    //remove a ultima linha add
+    tarefas.pop();
+    listarTarefas();
 }
