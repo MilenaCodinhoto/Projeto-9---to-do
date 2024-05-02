95d93eecb55ecdb33f25f20a4884f687e378560f
Milena <milena.cod@gmail.com>
2024-05-02 10:56:36 -0300

    crianco a função validar campo

---
+++ Desktop/projeto9/src/js/script.js
@@ -0,0 +1,13 @@
+//declarando a variavel
+
+let tarefa = [];
+
+//função que valida o preenchimento do campo caso esteja vazio
+
+function validaCampo(){
+    let valida = false;
+    if(document.getElementById("task").value =="");
+    valida = true;
+    return valida;
+}
+
